840620beb9ad4e0bcba16e324b4e499133f96283
Emilio Reguero <eregueroc@gmail.com>
2018-03-23 14:37:15 +0100

    Lenght!
    Por fin!

--- P2/js/funciones.js
+++ P2/js/funciones.js
@@ -15,11 +15,18 @@ function mostrarComentarios(){
 function validarEmail() {
     
     var email = document.getElementById("email").value;
+    var nombre = document.getElementById("entrada_nombre").value;
+    var texto = document.getElementById("entrada_texto").value;
 
     //validacion de email
     if (/^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i.test(email)){
-        //crea el comentario con texto sin palabras prohibidas
-        crearComentario();
+        if(nombre != "" && texto != ""){
+	        crearComentario();
+	        alert("¡Comentario enviado!");
+    	}
+    	else{
+    		alert("No has rellenado todos los datos.");
+    	}
     } else {
         alert("¡La dirección de email es incorrecta!");
     }
@@ -30,7 +37,7 @@ function validarTexto() {
 //Listado de palabras prohibidas
 	if (event.keyCode == 32){
 		var texto = document.getElementById("entrada_texto").value;
-        var prohibidas = ["mala", "pa^la^bra^2", "etc"];
+        var prohibidas = ["mierda", "basura", "puta", "gilipollas", "cabron", "tonto", "idiota", "caca", "holi", "tontos" ];
 
         //Generar el regex
         function escaparRegex(string) {
@@ -41,29 +48,37 @@ function validarTexto() {
         regex = new RegExp('\\[?\\b(?:' + prohibidasOr + ')\\b\\]?', 'gi');
 
     	//Reemplazar
-
-    	var x = '*';
-    	
-        resultado = texto.replace(regex, x);
-
-        document.getElementById("entrada_texto").value = resultado;
-    }
+    	var n = texto.split(" ");
+    	var word = n[n.length - 2];
+    	var x = "*";
+    	var finish = x.repeat(word.length);
+    	resultado = texto.replace(regex, finish);
+		document.getElementById("entrada_texto").value = resultado;
+	}
 }
 
 
 
 function crearComentario() {
   
+  //Cogemos el texto y nombre del formulario
   var texto_in = document.getElementById("entrada_texto").value;
   var nombre_in = document.getElementById("entrada_nombre").value;
 
+  //El divPrincipal es comentarios que es donde se contienen el comentario
   var divPrincipal= document.getElementById('comentarios') ;
+  //Creamos un nuevo div
   var newDiv = document.createElement("div");
 
+  //Creamos un nuevo p para elnombre
   var paraN = document.createElement("p");
+  //Asignamos la classe nombre a ese p
   paraN.className="nombre";
+  //creamos texto dentro de p con el nombre
   var nombre = document.createTextNode(nombre_in);
+  //insertamos el texto en p
   paraN.appendChild(nombre);
+
 
   var paraD = document.createElement("p");
   paraD.className="fecha";
