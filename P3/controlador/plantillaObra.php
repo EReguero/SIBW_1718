731e45122831d2b2d718900c0c98551437555ffc
Emilio Reguero <eregueroc@gmail.com>
2018-04-13 14:31:33 +0200

    BD + Comments

--- P3/controlador/plantillaObra.php
+++ P3/controlador/plantillaObra.php
@@ -39,7 +39,32 @@
                 include 'recursos/obra.php'; 
             ?> 
 
-             <div id="botonessociales">
+ 			<button id="boton_comentarios" onclick="mostrarComentarios()">Comentarios</button>
+            <div id ="box_comentarios">
+                <div id="comentarios">
+    	            	<div class="comentario">
+                            <p class="nombre">Emilio Reguero Castro</p>
+                            <p class="fecha">Tue Mar 20 2018 13:25:43 GMT+0100 (Hora estándar romance)</p>
+                            <p class="text">Es una obra preciosa, merece la pena ir a Bilbao a verla.</p>
+                        </div>
+                        <div class="comentario">
+                            <p class="nombre">Eric</p>
+                            <p class="fecha">Wed Mar 21 2018 18:45:33 GMT+0100 (Hora estándar romance)</p>
+                            <p class="text">
+                                Me gusta mucho esta obra, les aconsejé a mis amigos que vengan a ver este espectáculo de arte.</p>
+                        </div>
+                </div>
+                <div id="comentar">
+                    <label for="entrada_nombre">Nombre:  </label>
+                    <input type="text" name="entrada_nombre" id="entrada_nombre" placeholder="Nombre completo">
+                    <label for="email">Email:  </label>
+                    <input type="text" name="email" id="email" placeholder="example@email.es"> 
+                    <textarea name="entrada_texto" id="entrada_texto" placeholder="Introduzca aquí su comentario." onkeyup="validarTexto()"></textarea>
+                    <button id="crear_comentario" onclick="validarEmail()">Enviar</button>
+                </div>
+            </div>
+
+            <div id="botonessociales">
                 <!-- Botón de pagina para imprimir -->
                 <a href="obra_imprimir.html">
                     <img src="../img/print_button.jpg" alt="Boton imprimir">
