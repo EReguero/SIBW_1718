b8b607b2993f2b31467b97505b1d423245c6c8fc
Emilio Reguero <eregueroc@gmail.com>
2018-04-22 16:05:58 +0200

    Minor

--- P3/recursos/redes.php
+++ P3/recursos/redes.php
@@ -2,12 +2,16 @@
 <head>
 	<title>Compartir con <?php echo $_GET['red'];?></title>
 	  <script src="../js/funciones.js"></script>
+      <link rel="stylesheet" type="text/css" href="../css/stylecompartir.css"/>
 </head>
 <body>
-	<p>Se publicara en <?php echo $_GET['red']?> el siguiente mensaje</p>
-	<p> <?php echo $_GET['titulo']?> via @GuggenheimBilbao</p>
-	<img src="../<?php echo $_GET['imagen'] ?>" alt="<?php echo $_GET['titulo'] ?>">
-
+    <div id="publicacion">
+        <p>Se publicara en <?php echo $_GET['red']?> el siguiente mensaje</p>
+        <p> <?php echo $_GET['titulo']?> via @GuggenheimBilbao</p>
+    </div>
+	<div id="imageobracompartir">
+	    <img src="../<?php echo $_GET['imagen'] ?>" alt="<?php echo $_GET['titulo'] ?>">
+     </div>
 	<button onclick="window_close();">Aceptar</button>
 </body>
 </html>
